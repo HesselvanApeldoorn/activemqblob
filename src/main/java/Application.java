02cb23990509c369fe253c9a514c0dc3404264c2
hvapeldoorn <h.vapeldoorn@quintor.local>
2016-12-09 15:51:15 +0100

    added sending and receiving of blob

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,12 +1,12 @@
 import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.activemq.ActiveMQSession;
 import org.apache.activemq.BlobMessage;
+import org.apache.activemq.command.ActiveMQBlobMessage;
 
 import javax.jms.*;
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileWriter;
 import java.io.InputStream;
+import java.net.URL;
 
 /**
  * Created by hessel on 23-11-16.
@@ -21,40 +21,60 @@ public class Application {
     }
 
     private Application() throws Exception {
-        InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream("test.img");
-//        String xml = Application.convertStreamToString(inputStream);
-//        this.processMessage("aaa");
-//        this.processInputstream(inputStream);
-        this.alternative();
+        this.sendBlob();
+        this.receiveBlob();
     }
 
-    private void alternative()  throws Exception {
+
+    private void sendBlob()  throws Exception {
+
+        ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory("admin", "admin", "tcp://localhost:61616");
+//        ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory("admin", "admin", "tcp://localhost:61616?jms.blobTransferPolicy.defaultUploadUrl=http://localhost:8161/fileserver/");
+
+        //Establish the connection
+        Connection connection = factory.createConnection();
+
+        ActiveMQSession session = (ActiveMQSession) connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        Queue queue = session.createQueue("Test");
+        javax.jms.MessageProducer producer = session.createProducer(queue);
+        connection.start();
+
+        BlobMessage message = session.createBlobMessage(new URL("https://raw.githubusercontent.com/HesselvanApeldoorn/activemqblob/master/src/main/java/Application.java"));
+        message.setName("blob filename");
+
+        producer.send(message);
+        Thread.sleep(1000);
+        System.out.println("sent blob");
+
+    }
+
+
+    private void receiveBlob() throws Exception {
 
         ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory("admin", "admin", "tcp://localhost:61616");
         //Establish the connection
         Connection connection = factory.createConnection();
 
-        File file = File.createTempFile("amq-data-file-", ".dat");
-        // lets write some data
-        String content = "hello world " + System.currentTimeMillis();
-        BufferedWriter writer = new BufferedWriter(new FileWriter(file));
-        writer.append(content);
-        writer.close();
-
-        file = new File("/home/hessel/git/javatraining/untitled/src/main/resources/test.img");
-
         ActiveMQSession session = (ActiveMQSession) connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-        Queue queue = session.createQueue("Test3");
-        javax.jms.MessageProducer producer = session.createProducer(queue);
-        BlobMessage message = session.createBlobMessage(file);
+        Queue queue = session.createQueue("Test");
+        javax.jms.MessageConsumer consumer = session.createConsumer(queue);
+        connection.start();
 
-        message.setName("fileName");
+            Message message = consumer.receive();
 
-        producer.send(message);
-        Thread.sleep(1000);
+            if (message instanceof ActiveMQBlobMessage) {
+                ActiveMQBlobMessage blob = (ActiveMQBlobMessage) message;
+                System.out.println("found blob: " + blob.getRemoteBlobUrl());
+                Thread.sleep(1000);
+//                blob.deleteFile();
+                this.processInputstream(new URL(blob.getRemoteBlobUrl()));
+            } else {
+                System.out.println(" found general message: " + message.getJMSMessageID());
+            }
+
     }
 
-    private void processInputstream(InputStream inputStream) {
+    private void processInputstream(URL url) {
         try {
             ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory("admin", "admin", "tcp://localhost:61616");
             //Establish the connection
@@ -62,10 +82,12 @@ public class Application {
             ActiveMQSession session = (ActiveMQSession) connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
             Queue queue = session.createQueue("Test2");
             javax.jms.MessageProducer producer = session.createProducer(queue);
-            BlobMessage message = session.createBlobMessage(inputStream);
+            connection.start();
+            BlobMessage message = session.createBlobMessage(url);
 
             producer.send(message);
-            System.out.println("sent blob message");
+            System.out.println("sent blob from queue" + queue.getQueueName());
+            System.out.println(" blob content: " + Application.convertStreamToString(message.getInputStream()));
         } catch ( Exception e) {
             e.printStackTrace();
         }
@@ -74,27 +96,5 @@ public class Application {
     static String convertStreamToString(java.io.InputStream is) {
         java.util.Scanner s = new java.util.Scanner(is).useDelimiter("\\A");
         return s.hasNext() ? s.next() : "";
-    }
-
-
-    private void processMessage(String xml) {
-        try {
-            //created ConnectionFactory object for creating connection
-            ConnectionFactory factory = new ActiveMQConnectionFactory("admin", "admin", "tcp://localhost:61616");
-            //Establish the connection
-            Connection connection = factory.createConnection();
-            ActiveMQSession session = (ActiveMQSession) connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-            Queue queue = session.createQueue("Test");
-            //Added as a producer
-            javax.jms.MessageProducer producer = session.createProducer(queue);
-            // Create and send the message
-            TextMessage msg = session.createTextMessage();
-            msg.setText(xml);
-            producer.send(msg);
-            System.out.println("sent message");
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 }
